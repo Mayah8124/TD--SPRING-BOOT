4d067c8eca3b12b30282c938f664e08141b60180
mayahNeko <mayahibela@gmail.com>
2026-03-18 16:10:52 +0300

    feat: Question A

--- src/main/java/com/spring_boot/td/controller/WelcomeController.java
+++ src/main/java/com/spring_boot/td/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.spring_boot.td.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name){
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name){
+        if ("".equals(name)){
+            return ResponseEntity.status(400).build();
+        } else  {
+            return ResponseEntity.status(200).body("Welcome " + name);
+        }
     }
 
 }
