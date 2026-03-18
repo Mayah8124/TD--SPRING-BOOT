11f134d74142bc0c464fc6ac789ad1f4097f2f08
mayahNeko <mayahibela@gmail.com>
2026-03-18 14:23:05 +0300

    feat: create initial class

---
+++ src/main/java/com/spring_boot/td/StudentController.java
@@ -0,0 +1,15 @@
+package com.spring_boot.td;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @PostMapping("/students")
+    public List<Students> createStudent () {
+
+    }
+}
--- src/main/java/com/spring_boot/td/WelcomeController.java
+++ src/main/java/com/spring_boot/td/WelcomeController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class td2Controller {
+public class WelcomeController {
 
     @GetMapping("/welcome")
     public String welcome(@RequestParam String name){
         return "Welcome " + name;
     }
+
 }
