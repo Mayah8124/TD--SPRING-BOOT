069cf22876fe560243cd6364d90f27abd8ad5e0e
mayahNeko <mayahibela@gmail.com>
2026-03-18 15:43:45 +0300

    feat: Question C

--- src/main/java/com/spring_boot/td/controller/StudentController.java
+++ src/main/java/com/spring_boot/td/controller/StudentController.java
@@ -1,15 +1,31 @@
 package com.spring_boot.td.controller;
 
 import com.spring_boot.td.entity.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
+
+    private final List<Student> studentList = new ArrayList<>();
+
+    @GetMapping("/students")
+    public String getStudentsName(@RequestHeader (value = "Accept", defaultValue = "text/plain") String Accept) {
+        if (!"text/plain".equalsIgnoreCase(Accept)) {
+            return "Format non supporté";
+        }
+
+        if (studentList.isEmpty()) {
+            return "Student list is empty";
+        }
+
+        return studentList.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.joining(", "));
+    }
 
     @PostMapping("/students")
     public List<Student> createStudent (@RequestBody List<Student> students) {
