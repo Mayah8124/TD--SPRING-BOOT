af3129550b4d6760df16c88771ab6b898d1473a1
mayahNeko <mayahibela@gmail.com>
2026-03-18 15:24:45 +0300

    feat: Question B

--- src/main/java/com/spring_boot/td/controller/StudentController.java
+++ src/main/java/com/spring_boot/td/controller/StudentController.java
@@ -2,15 +2,23 @@ package com.spring_boot.td.controller;
 
 import com.spring_boot.td.entity.Student;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentController {
 
     @PostMapping("/students")
-    public List<Student> createStudent () {
+    public List<Student> createStudent (@RequestBody List<Student> students) {
+        List<Student> studentList = new ArrayList<>();
 
+        for (Student student : students) {
+            studentList.add(student);
+        }
+
+        return studentList;
     }
 }
