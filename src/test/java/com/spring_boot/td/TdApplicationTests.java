00cebb1e6196aa5dd7b230a97124e4b83a95a23b
mayahNeko <mayahibela@gmail.com>
2026-03-18 13:55:46 +0300

    Initialize project

---
+++ src/test/java/com/spring_boot/td/TdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.td;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
